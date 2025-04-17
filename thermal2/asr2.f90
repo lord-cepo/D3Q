d17b3d7d8042b749808947303c543605c5001982
lord_cepo <alessandro.ciavatta2@studio.unibo.it>
2025-04-17 17:19:01 +0200

    introduced a new sum rule, to be tested for gamma

--- thermal2/asr2.f90
+++ thermal2/asr2.f90
@@ -8,8 +8,8 @@
 MODULE asr2_module
 
   USE kinds,    ONLY : DP
-  
-  CONTAINS
+
+CONTAINS
   SUBROUTINE impose_asr2(method, nat,fc,zeu)
     USE input_fc,       ONLY : forceconst2_grid
     IMPLICIT NONE
@@ -21,12 +21,42 @@ MODULE asr2_module
     !
     INTEGER :: iR, a,b, i,j, nu,mu,mu_, na
     REAL(DP):: delta, norm
+    real(dp), dimension(3,3,nat) :: delta1, delta2
     !
     IF(method=="no" .or. method=="none")RETURN
     !
     ! Simple Acoustic Sum Rule on force constants in real space
     !
-    IF(method=="simple")THEN
+    IF(method=="diff") then
+      do i = 1, nat
+        do b = 1, 3
+          do a = 1, 3
+            delta1(a,b,i) = 0._dp
+            delta2(a,b,i) = 0._dp
+            do j = 1, nat
+              do iR = 1, fc%n_R
+                delta1(a,b,i) = delta1(a,b,i) + fc%FC(3*(i-1)+a,3*(j-1)+b,iR)
+                delta2(a,b,i) = delta2(a,b,i) + fc%FC(3*(j-1)+b,3*(i-1)+a,iR)
+              enddo
+            enddo
+          enddo
+        enddo
+      enddo
+      delta1 = delta1 / nat / fc%n_R / 2
+      delta2 = delta2 / nat / fc%n_R / 2
+      do i = 1, nat
+        do b = 1, 3
+          do a = 1, 3
+            do j = 1, nat
+              do iR = 1, fc%n_R
+                fc%FC(3*(i-1)+a,3*(j-1)+b,iR) = &
+                  fc%FC(3*(i-1)+a,3*(j-1)+b,iR) - delta1(a,b,i) - delta2(a,b,j)
+              enddo
+            enddo
+          enddo
+        enddo
+      enddo
+    ELSEIF(method=="simple")THEN
       DO a = 1,3
         DO b = 1,3
           DO i = 1,nat
@@ -42,8 +72,8 @@ MODULE asr2_module
             !write(*,*) 'asr2', a,b,i, delta
             mu_=  3*(i-1)+b
             fc%FC(nu,mu_,fc%i_0)=fc%FC(nu,mu_,fc%i_0)-delta
-          ENDDO  
-        ENDDO    
+          ENDDO
+        ENDDO
       ENDDO
 
     ELSEIF(method=="diagonal")THEN
@@ -59,7 +89,7 @@ MODULE asr2_module
               mu = 3*(j-1)+b
               DO iR = 1,fc%n_R
                 delta = delta+fc%FC(mu,nu,iR)
-                !norm  = norm +fc%FC(mu,nu,iR)**2 
+                !norm  = norm +fc%FC(mu,nu,iR)**2
               ENDDO
             ENDDO
             mu = 3*(i-1)+b
@@ -74,67 +104,67 @@ MODULE asr2_module
         ENDDO
       ENDDO
     ELSEIF(method=="spread")THEN
-      !CALL errore("impose_asr2", "spread asr is buggy/wrong"//TRIM(method), 1) 
+      !CALL errore("impose_asr2", "spread asr is buggy/wrong"//TRIM(method), 1)
       DO i = 1,nat
-       DO a = 1,3
-       DO b = 1,3
-       nu = 3*(i-1)+a
-         !
-         delta = 0._dp
-         !norm = 0._dp
-         !
-         DO j = 1,nat
-         mu = 3*(j-1)+b
-         DO iR = 1,fc%n_R
-           delta = delta+fc%FC(mu,nu,iR)
-           !norm  = norm +fc%FC(mu,nu,iR)**2 
-         ENDDO
-         ENDDO      
-           norm = 0._dp
-           DO j = 1,nat
-           mu = 3*(j-1)+b
-           DO iR = 1,fc%n_R
-             norm  = norm +fc%FC(mu,nu,iR)**2 
-           ENDDO
-           ENDDO
-           !  
-           DO j = 1,nat
-           mu = 3*(j-1)+b
-           DO iR = 1,fc%n_R
-             fc%FC(mu,nu,iR) = fc%FC(mu,nu,iR) - delta*fc%FC(mu,nu,iR)**2/norm
-           ENDDO
-           ENDDO
-       ENDDO
-       ENDDO
+        DO a = 1,3
+          DO b = 1,3
+            nu = 3*(i-1)+a
+            !
+            delta = 0._dp
+            !norm = 0._dp
+            !
+            DO j = 1,nat
+              mu = 3*(j-1)+b
+              DO iR = 1,fc%n_R
+                delta = delta+fc%FC(mu,nu,iR)
+                !norm  = norm +fc%FC(mu,nu,iR)**2
+              ENDDO
+            ENDDO
+            norm = 0._dp
+            DO j = 1,nat
+              mu = 3*(j-1)+b
+              DO iR = 1,fc%n_R
+                norm  = norm +fc%FC(mu,nu,iR)**2
+              ENDDO
+            ENDDO
+            !
+            DO j = 1,nat
+              mu = 3*(j-1)+b
+              DO iR = 1,fc%n_R
+                fc%FC(mu,nu,iR) = fc%FC(mu,nu,iR) - delta*fc%FC(mu,nu,iR)**2/norm
+              ENDDO
+            ENDDO
+          ENDDO
+        ENDDO
       ENDDO
     ELSE
       CALL errore("impose_asr2", "unknown method "//TRIM(method), 1)
     ENDIF
     !
-    ! Simple Acoustic Sum Rule on effective charges, in practice this 
+    ! Simple Acoustic Sum Rule on effective charges, in practice this
     ! should not be needed because already applied by d3_q2r.x
     ! see below debug line
     !
     IF(present(zeu)) THEN
-     do i=1,3
+      do i=1,3
         do j=1,3
-           delta=0._dp
-           do na=1,nat
-              delta = delta + zeu(i,j,na)
-           end do
-           !! DEBUG
-           IF (ABS(delta)>1e-8) THEN
-            ! this should never appear, since the asr on the effective charges is 
-            ! imposed in d3_q2r. Limit to 1E-8 because the effective charges are 
+          delta=0._dp
+          do na=1,nat
+            delta = delta + zeu(i,j,na)
+          end do
+          !! DEBUG
+          IF (ABS(delta)>1e-8) THEN
+            ! this should never appear, since the asr on the effective charges is
+            ! imposed in d3_q2r. Limit to 1E-8 because the effective charges are
             ! written with a precision of 1E-16 and have typical value of the order 1E0
             print*, "DEBUG zeu asr, check delta=", delta
-           ENDIF
-           !
-           do na=1,nat
-              zeu(i,j,na) = zeu(i,j,na) - delta/nat
-           end do
+          ENDIF
+          !
+          do na=1,nat
+            zeu(i,j,na) = zeu(i,j,na) - delta/nat
+          end do
         end do
-     end do
+      end do
     ENDIF
     !
   END SUBROUTINE impose_asr2
