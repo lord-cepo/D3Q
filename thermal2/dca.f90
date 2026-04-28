fb3ea4d6894e356111b16a4d1ced239ba86ef2fa
lord-cepo <lord-cepo>
2026-04-28 09:55:14 +0200

    ennesimo commit, non so perché bisogna rinormalizzare self_uncoarsed per 1/NQ

--- thermal2/dca.f90
+++ thermal2/dca.f90
@@ -787,7 +787,7 @@ contains
     !
     open(115, file='self_uncoarsed.dat')
     do iw = 1, input%n_omega
-      if(ionode) write(115, "(100E20.8)") wg%en(iw), self_uncoarsed(:,iw)
+      if(ionode) write(115, "(100E20.8)") wg%en(iw)*RY_TO_CMM1, wg%en(iw)*RY_TO_CMM1, self_uncoarsed(:,iw)*RY_TO_CMM1**2
     enddo
     close(115)
     !
